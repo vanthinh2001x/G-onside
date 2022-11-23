303f1b360e91923299def1369d40276a3949e6ac
Thinh Pham <phamvanthinh2001x@gmail.com>
2022-11-23 11:08:18 +0700

    🔍 create search tab

---
+++ components/SearchBar.js
@@ -0,0 +1,18 @@
+import { View, Text, TextInput } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import React from "react";
+
+const SearchBar = ({ onSearchChange, inputPlaceHolder }) => {
+  return (
+    <View className="flex-row w-full h-10 items-center bg-[#e5e4ea] py-2 px-4 rounded-3xl">
+      <Icon name="search" size={20} color="#9ca3af" />
+      <TextInput
+        onChangeText={(text) => console.log(text)}
+        placeholder={inputPlaceHolder}
+        className="flex-1 ml-2 text-base leading-5 text-gray-900"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
